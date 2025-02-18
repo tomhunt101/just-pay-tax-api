cc663542abba0160ca341c51d547dc0e3ad0f57b
tomhunt101 <tj-hunt@hotmail.co.uk>
2025-02-18 14:51:17 +0000

    Update app.py

--- app.py
+++ app.py
@@ -14,4 +14,4 @@ def tax_counter():
     return jsonify({"total_tax_evasion": round(total_evasion, 2)})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8080)
